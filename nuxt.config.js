3e18df33c438198329ab7aa0ef7e9e49d73545b3
OgbeniHMMD <olajide.a.hammed@gmail.com>
2021-03-18 14:25:26 +0100

    Purge:
    - Purged unneded modules/plugins

--- nuxt.config.js
+++ nuxt.config.js
@@ -51,12 +51,7 @@ export default {
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-    // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
-    // https://go.nuxtjs.dev/content
-    '@nuxt/content',
-  ],
+  modules: [],
 
   tailwindcss: {
     jit: true,
